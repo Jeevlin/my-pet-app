072aef7e119c6c574bd032da836b92b42e2d90ff
Jeevlin <princessjeev@gmail.com>
2025-09-01 16:35:51 +0530

    error toggle

--- petstore-frontend/vite-project/src/homepage/homepage.jsx
+++ petstore-frontend/vite-project/src/homepage/homepage.jsx
@@ -313,18 +313,7 @@ const handleFindOrder = async () => {
                         </div>
                     </div>
                 </div>
-                    <ToastContainer 
-      position="top-center"
-      autoClose={3000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="colored"
-    />
+                    <ToastContainer />
             </div>
 
     </div>
